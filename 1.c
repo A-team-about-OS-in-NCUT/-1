ace4764618a4042bfc4e103aa321529e48bf148e
whiteicey <53032014+whiteicey@users.noreply.github.com>
2021-03-04 17:09:23 +0800

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-#include <fcntl.h>
+#include <stdlib.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
 	char oldname[100], newname[100];
 	printf("请告诉我一个文件的完整路径: ");
 	gets(oldname);
 	printf("您想修改为: ");
 	gets(newname);
+	if (argc != 3) {
+		printf("1.c %s %s", oldname, newname);
+		exit(1);
+	}
 	if (rename(oldname, newname) == 0)
 		printf("已经把文件 %s 修改为 %s.\n", oldname, newname);
 	else
